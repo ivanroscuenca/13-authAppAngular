7f5c97589ba10076b5b65d260a64483451ba7cb7
ivanros <ivanros@protonmail.com>
2023-10-04 11:13:17 +0200

    video358

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  //llamamos a nuestra variable de entorno
+  private readonly baseUrl:string = environment.baseUrl;
+//importamos httpclient para las peticiones http
+  private http = inject(HttpClient);
+
+  //utilizaremos las señales(signals)
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>();
+
   constructor() { }
+
+  login(email:string,password:string): Observable<boolean>{
+    return of(true);
+  }
 }
